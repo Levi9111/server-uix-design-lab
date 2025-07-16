93b9e0c037e142fc8580ff65793016316ff172c1
levi9111 <captainlevi9111@gmail.com>
2025-07-16 18:45:05 +0600

    link for cors updated

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,14 @@ import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 //parsers
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: ['https://dashboard-uix-design-lab.vercel.app/'],
+    credentials: true,
+  }),
+);
+
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
