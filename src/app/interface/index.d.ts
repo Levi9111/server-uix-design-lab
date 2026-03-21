1470c489ed785ca0ae5b613b54528e79a3e977b9
levi9111 <captainlevi9111@gmail.com>
2026-03-21 11:19:31 +0600

    feat: Merge IJwtPayload into the Express.User interface for better type recognition.

--- src/app/interface/index.d.ts
+++ src/app/interface/index.d.ts
@@ -2,6 +2,8 @@ import { IJwtPayload } from '../modules/Auth/auth.interface';
 
 declare global {
   namespace Express {
+    // Merge IJwtPayload into Express.User so req.user.userId etc. are recognized
+    interface User extends IJwtPayload {}
     interface Request {
       user?: IJwtPayload;
     }
