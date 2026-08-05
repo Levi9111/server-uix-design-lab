0863ec1efa100ee0dfd720f9dd09a34013cf9809
levi9111 <captainlevi9111@gmail.com>
2026-08-05 10:54:19 +0600

    feat: add Cloudinary configuration and super admin credentials to environment variables

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -12,4 +12,9 @@ export default {
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_url: process.env.CLOUDINARY_URL,
+  super_admin_email: process.env.SUPER_ADMIN_EMAIL,
+  super_admin_password: process.env.SUPER_ADMIN_PASSWORD,
 };
